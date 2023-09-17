91216066308f0dae5b1b382f06463bfd3e4d0563
ValentinaMeca <144333202+ValentinaMeca@users.noreply.github.com>
2023-09-17 18:11:05 -0400

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //used to include a file in our program and we need it to use count
+
+int main() { //start a function
+    std::cout << "Hello World!"; //this prints out the contents of the program
+    return 0; //this exits the program kind of like sc.close
+}
