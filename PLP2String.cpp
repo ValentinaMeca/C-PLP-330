84678a4537f9a7df4f18bf3bf98e01af2adc911e
ValentinaMeca <144333202+ValentinaMeca@users.noreply.github.com>
2023-11-20 16:00:34 -0500

    Add files via upload

---
+++ PLP2String.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //used to include a file in our program and we need it to use count
+#include <string> //used to include a file in our program and we need it to use count
+int main ()
+{
+  std::string string1;
+  string1 = "Hi Everyone, how are you ? ";
+  std::cout << string1;
+  return 0;
+}
