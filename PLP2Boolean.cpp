fabe937447f7743c197ceab64c1f71a3c27644f6
ValentinaMeca <144333202+ValentinaMeca@users.noreply.github.com>
2023-11-20 15:54:34 -0500

    Add files via upload

---
+++ PLP2Boolean.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //used to include a file in our program and we need it to use count
+int main(){
+bool pinkismyfavcolor = true; //bool takes in the value of 1 (true) and 0 (false)
+bool yellowismyfavcolor = false;
+
+std::cout << pinkismyfavcolor;
+std::cout << yellowismyfavcolor;
+
+int a = 30;
+int b = 100; 
+std::cout << (a>b); // will return 0 because this is false 
+}
