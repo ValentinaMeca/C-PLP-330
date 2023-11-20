8e9ff2f80e284d4e1164df00330f2498fb60f203
ValentinaMeca <144333202+ValentinaMeca@users.noreply.github.com>
2023-11-20 15:55:48 -0500

    Add files via upload

---
+++ PLP2int.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //used to include a file in our program and we need it to use count
+int main() { //start a function
+    std::cout << "Welcome to PLP 2\n"; //this prints a string 
+    int a, b; //initializes variables
+    int sum; //delcares sum as as int
+    b = 5; //gives b a value
+    a = 10; //gives a the value of 10
+    sum = b + a; //to get sum add the ints
+    std::cout << sum; //prints sum
+    return 0; //this exits the program kind of like sc.close
+}
